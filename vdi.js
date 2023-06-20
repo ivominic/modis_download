107fba2e0ec10398b1fb292b97ff2ab9c9a51268
Ivo <ivo.minic@gmail.com>
2023-06-20 10:24:15 +0200

    Downloading both modis files and converting to tif

--- vdi.js
+++ vdi.js
@@ -7,17 +7,20 @@ const token =
   "eyJ0eXAiOiJKV1QiLCJvcmlnaW4iOiJFYXJ0aGRhdGEgTG9naW4iLCJzaWciOiJlZGxqd3RwdWJrZXlfb3BzIiwiYWxnIjoiUlMyNTYifQ.eyJ0eXBlIjoiVXNlciIsInVpZCI6Iml2by5taW5pYyIsImV4cCI6MTY5MjM0ODQxOSwiaWF0IjoxNjg3MTY0NDE5LCJpc3MiOiJFYXJ0aGRhdGEgTG9naW4ifQ.1OT1RZv5jAFEi0NfIAAzXg-5DN_qZaG1OC2HOHynL-0Qnb4Xlx4AxZiJb3pn4JUJRH6VDWnY6P4cf4DumWdacS7sxk5GIRBsFDYV_uYHTHIEFFhijgVEydyq6J5WRr55nG9blVv8n9bnuM3RBoz4C1XmGAxsL6nAgzwWg50nItc0MtIfN3jCurf96lR8BqWxPewn9_ZUAdNE7hQYYBDYs9hCwc5UfWVJgznYHsHRoQpUzklB2EzqMG0ajEdtTE-mJi07BIbubtxGIXngDWHjlkP3tKYPLSeS66rewfP8XfNi0-NiicHiSIBtZ_icbkbrxDz-W5zNI9rJSXIEeiRPGg";
 let url = "https://e4ftl01.cr.usgs.gov/MOLT/MOD13Q1.061/";
 let fileUrl = "";
+let fileModisUrl = "";
 let originalFilename = "";
 let blnExistFile = false; //Need to stop iterating when this value is true
 let blnFound = false; //Same as above, but for MOD09GA.061
+let vdiFileName = "temp.tif",
+  ndiiFileName = "temp.tif";
 
 const fileIdentifier = ".h19v04.";
 const fileExtension = ".hdf";
-const downloadFolder = "C:/Users/PC/Desktop/modis/vdi/hdf/";
+const downloadFolder = "C:/Users/PC/Desktop/modis/vdi/";
+const downloadModisFolder = "C:/Users/PC/Desktop/modis/ndii/";
 
 let urlModis = "https://e4ftl01.cr.usgs.gov/MOLT/MOD09GA.061/";
 let originalModisFilename = "";
-const downloadModisFolder = "C:/Users/PC/Desktop/modis/ndii/";
 
 //TODO: Check if file is already downloaded.
 //TODO: Create path regarding selected date.
@@ -67,6 +70,7 @@ iterateDates();
 async function iterateDates() {
   let loop = new Date();
   while (!blnExistFile) {
+    vdiFileName = `${formatDate(loop)}.tif`;
     console.log("Date", formatDate(loop));
     await scrapeData(url + formatDate(loop) + "/");
 
@@ -87,6 +91,7 @@ function formatDate(date) {
 async function getLatestModis09A1() {
   let loop = new Date();
   while (!blnFound) {
+    ndiiFileName = `${formatDate(loop)}.tif`;
     console.log("Date", formatDate(loop));
     await scrapeDataModis09A1(urlModis + formatDate(loop) + "/");
 
@@ -105,15 +110,48 @@ async function scrapeDataModis09A1(tempUrl) {
         originalModisFilename = $(el).text();
         console.log("FileName09A", originalModisFilename);
         blnFound = true;
-        fileUrl = tempUrl + originalModisFilename;
+        fileModisUrl = tempUrl + originalModisFilename;
         originalModisFilename = originalModisFilename;
       }
     });
     if (blnFound) {
       console.log("URL09A: ", tempUrl);
-      await downloadFile(fileUrl, token, downloadModisFolder + originalModisFilename);
+      await downloadFile(fileModisUrl, token, downloadModisFolder + originalModisFilename);
+      setTimeout(convertToTiff, 90000);
+      //await convertToTiff();
     }
   } catch (err) {
     //console.error("Error", err);
   }
+}
+
+async function convertToTiff() {
+  let command = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadFolder}${originalFilename}":MODIS_Grid_16DAY_250m_500m_VI:"250m 16 days NDVI" ${downloadFolder}tif/${vdiFileName}`;
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.log("ERROR", err);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+
+  let commandNdii2 = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadModisFolder}${originalModisFilename}":MODIS_Grid_500m_2D:sur_refl_b02_1 ${downloadModisFolder}tif/b2${ndiiFileName}`;
+  exec(commandNdii2, (err, stdout, stderr) => {
+    if (err) {
+      console.log("ERROR", err);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+  let commandNdii6 = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadModisFolder}${originalModisFilename}":MODIS_Grid_500m_2D:sur_refl_b06_1 ${downloadModisFolder}tif/b6${ndiiFileName}`;
+  exec(commandNdii6, (err, stdout, stderr) => {
+    if (err) {
+      console.log("ERROR", err);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }
