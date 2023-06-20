315cf81571b7f2a5ddb18d238f9488c7b06ce830
Ivo <ivo.minic@gmail.com>
2023-06-20 11:18:58 +0200

    Import into PostGIS

--- vdi.js
+++ vdi.js
@@ -3,6 +3,8 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const { exec } = require("child_process");
 
+const pgUserName = "postgres";
+const pgPassword = "postgres";
 const token =
   "eyJ0eXAiOiJKV1QiLCJvcmlnaW4iOiJFYXJ0aGRhdGEgTG9naW4iLCJzaWciOiJlZGxqd3RwdWJrZXlfb3BzIiwiYWxnIjoiUlMyNTYifQ.eyJ0eXBlIjoiVXNlciIsInVpZCI6Iml2by5taW5pYyIsImV4cCI6MTY5MjM0ODQxOSwiaWF0IjoxNjg3MTY0NDE5LCJpc3MiOiJFYXJ0aGRhdGEgTG9naW4ifQ.1OT1RZv5jAFEi0NfIAAzXg-5DN_qZaG1OC2HOHynL-0Qnb4Xlx4AxZiJb3pn4JUJRH6VDWnY6P4cf4DumWdacS7sxk5GIRBsFDYV_uYHTHIEFFhijgVEydyq6J5WRr55nG9blVv8n9bnuM3RBoz4C1XmGAxsL6nAgzwWg50nItc0MtIfN3jCurf96lR8BqWxPewn9_ZUAdNE7hQYYBDYs9hCwc5UfWVJgznYHsHRoQpUzklB2EzqMG0ajEdtTE-mJi07BIbubtxGIXngDWHjlkP3tKYPLSeS66rewfP8XfNi0-NiicHiSIBtZ_icbkbrxDz-W5zNI9rJSXIEeiRPGg";
 let url = "https://e4ftl01.cr.usgs.gov/MOLT/MOD13Q1.061/";
@@ -11,8 +13,9 @@ let fileModisUrl = "";
 let originalFilename = "";
 let blnExistFile = false; //Need to stop iterating when this value is true
 let blnFound = false; //Same as above, but for MOD09GA.061
-let vdiFileName = "temp.tif",
-  ndiiFileName = "temp.tif";
+let vdiFileName = "temp",
+  ndiiFileNameB2 = "temp",
+  ndiiFileNameB6 = "temp";
 
 const fileIdentifier = ".h19v04.";
 const fileExtension = ".hdf";
@@ -70,7 +73,7 @@ iterateDates();
 async function iterateDates() {
   let loop = new Date();
   while (!blnExistFile) {
-    vdiFileName = `${formatDate(loop)}.tif`;
+    vdiFileName = `${formatDate(loop)}`.replaceAll(".", "");
     console.log("Date", formatDate(loop));
     await scrapeData(url + formatDate(loop) + "/");
 
@@ -91,7 +94,8 @@ function formatDate(date) {
 async function getLatestModis09A1() {
   let loop = new Date();
   while (!blnFound) {
-    ndiiFileName = `${formatDate(loop)}.tif`;
+    ndiiFileNameB2 = `b2${formatDate(loop)}`.replaceAll(".", "");
+    ndiiFileNameB6 = `b6${formatDate(loop)}`.replaceAll(".", "");
     console.log("Date", formatDate(loop));
     await scrapeDataModis09A1(urlModis + formatDate(loop) + "/");
 
@@ -126,32 +130,55 @@ async function scrapeDataModis09A1(tempUrl) {
 }
 
 async function convertToTiff() {
-  let command = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadFolder}${originalFilename}":MODIS_Grid_16DAY_250m_500m_VI:"250m 16 days NDVI" ${downloadFolder}tif/${vdiFileName}`;
+  let command = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadFolder}${originalFilename}":MODIS_Grid_16DAY_250m_500m_VI:"250m 16 days NDVI" ${downloadFolder}tif/${vdiFileName}.tif`;
   exec(command, (err, stdout, stderr) => {
     if (err) {
       console.log("ERROR", err);
       return;
     }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
   });
 
-  let commandNdii2 = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadModisFolder}${originalModisFilename}":MODIS_Grid_500m_2D:sur_refl_b02_1 ${downloadModisFolder}tif/b2${ndiiFileName}`;
+  let commandNdii2 = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadModisFolder}${originalModisFilename}":MODIS_Grid_500m_2D:sur_refl_b02_1 ${downloadModisFolder}tif/${ndiiFileNameB2}.tif`;
   exec(commandNdii2, (err, stdout, stderr) => {
     if (err) {
       console.log("ERROR", err);
       return;
     }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
   });
-  let commandNdii6 = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadModisFolder}${originalModisFilename}":MODIS_Grid_500m_2D:sur_refl_b06_1 ${downloadModisFolder}tif/b6${ndiiFileName}`;
+  let commandNdii6 = `gdal_translate -of GTiff HDF4_EOS:EOS_GRID:"${downloadModisFolder}${originalModisFilename}":MODIS_Grid_500m_2D:sur_refl_b06_1 ${downloadModisFolder}tif/${ndiiFileNameB6}.tif`;
   exec(commandNdii6, (err, stdout, stderr) => {
     if (err) {
       console.log("ERROR", err);
       return;
     }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+  });
+
+  setTimeout(importTiffToPostgis, 20000);
+}
+
+async function importTiffToPostgis() {
+  console.log("START IMPORTING TO PostGIS");
+  let command = `raster2pgsql -F -I -C -t 4800x4800  ${downloadFolder}tif/${vdiFileName}.tif modisvdm.${vdiFileName} | psql postgresql://${pgUserName}:${pgPassword}@localhost:5432/nffis`;
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.log("ERROR", err);
+      return;
+    }
+  });
+
+  let commandNdii2 = `raster2pgsql -F -I -C -t 2400x2400  ${downloadModisFolder}tif/${ndiiFileNameB2}.tif modisvdm.${ndiiFileNameB2} | psql ${pgUserName}://${pgPassword}:postgres@localhost:5432/nffis`;
+  exec(commandNdii2, (err, stdout, stderr) => {
+    if (err) {
+      console.log("ERROR", err);
+      return;
+    }
+  });
+
+  let commandNdii6 = `raster2pgsql -F -I -C -s 2400x2400  ${downloadModisFolder}tif/${ndiiFileNameB6}.tif modisvdm.${ndiiFileNameB6} | psql ${pgUserName}://${pgPassword}:postgres@localhost:5432/nffis`;
+  exec(commandNdii6, (err, stdout, stderr) => {
+    if (err) {
+      console.log("ERROR", err);
+      return;
+    }
   });
 }
